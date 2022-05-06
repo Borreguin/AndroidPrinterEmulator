e99d4edc18ac178a89f7244344072694205c445b
Roberto Sanchez <roberto.sanchez@formos.com>
2022-05-06 10:06:24 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,186 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import TcpSocket from 'react-native-tcp-socket';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { EscCharacters } from './symbols';
+
+
+
+
+
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+
+
+  const server = TcpSocket.createServer(function(socket) {
+    socket.on('data', (data) => {
+      socket.write('Echo server ' + data);
+    });
+  
+    socket.on('error', (error) => {
+      console.log('An error ocurred with client socket ', error);
+    });
+  
+    socket.on('close', (error) => {
+      console.log('Closed connection with ', socket.address());
+    });
+
+   
+  }).listen({ port: 9100, host: '0.0.0.0' });
+  
+
+  const decode = (data) =>{
+    let resp = "";
+    for(var d of data){
+      const letter = String.fromCharCode(d);
+      if(!EscCharacters.includes(letter)){
+        resp += letter;
+      }
+    }
+    
+    return resp;
+  }
+
+  function toHex(str,hex){
+    try{
+      hex = unescape(encodeURIComponent(str))
+      .split('').map(function(v){
+        return v.charCodeAt(0).toString(16)
+      }).join('')
+    }
+    catch(e){
+      hex = str
+      console.log('invalid text input: ' + str)
+    }
+    return hex
+  }
+  
+
+  server.on('connection', (socket) => {
+    console.log('Client connected to server on ' + JSON.stringify(socket.address()));
+
+    socket.on('data', (data) => {
+        // console.log('Server client received: ' + (data.length < 2500 ? data : data.length + ' bytes'));
+        console.log('Server client received: ' + (data.length < 25000 ? decode(data) : data.length + ' bytes'));
+    });
+
+    socket.on('error', (error) => {
+        console.log('Server client error ' + error);
+    });
+
+    socket.on('close', (error) => {
+        console.log('Server client closed ' + (error ? error : ''));
+    });
+});
+
+  server.on('error', (error) => {
+    console.log('An error ocurred with the server', error);
+  });
+  
+  server.on('close', () => {
+    console.log('Server closed connection');
+  });
+
+  
+
+
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          {/* <Button title="Press me" onPress={newFunction} >*/}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ symbols.js
@@ -0,0 +1,41 @@
+class SpecialSymbol{
+    symbol;
+    description;
+    replaceWith;
+    constructor(symbol, replaceWith, description){
+        this.symbol = symbol;
+        this.replaceWith = replaceWith;
+        this.description = description;
+    }
+
+}
+
+const STX = new SpecialSymbol(String.fromCharCode(0x02), "", "STX"); 
+const DLE = new SpecialSymbol(String.fromCharCode(0x10), "", "DLE"); 
+const ESC = new SpecialSymbol(String.fromCharCode(0x1B), "", "SCAPE"); 
+const FS = new SpecialSymbol(String.fromCharCode(0x1C), "", "FS"); 
+const GS = new SpecialSymbol(String.fromCharCode(0x1D), "", "GS"); 
+
+const CAN = new SpecialSymbol(String.fromCharCode(0x18), "", "CAN");
+const FF = new SpecialSymbol(String.fromCharCode(0x0C), "", "Print and feed paper 1 lines");
+const SP = new SpecialSymbol(String.fromCharCode(0x20), " ", "Space");
+
+const AT = new SpecialSymbol(String.fromCharCode(0x40), "", "@")
+
+const C_EOT = new SpecialSymbol(String.fromCharCode(0x04), "", "C_EOT");
+const C_END = new SpecialSymbol(String.fromCharCode(0x05),"", "C_END");
+const C_DC4 = new SpecialSymbol(String.fromCharCode(0x14), "", "C_DC4");
+
+
+// const C_DC41 = C_DC4 + '1';
+// const C_DC42 = C_DC4 + '2';
+// const C_DC48 = C_DC4 + '8';
+
+export const EscCharacters = [
+    STX.symbol, DLE.symbol, ESC.symbol, FS.symbol, GS.symbol, CAN.symbol, FF.symbol, AT.symbol, C_EOT.symbol, C_END.symbol, C_DC4.symbol
+]
+
+
+
+
+// List of commands:
