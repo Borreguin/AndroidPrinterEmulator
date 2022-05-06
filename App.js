5ee6e06f4efa62f7d39971637fdf500821ce3b8b
Roberto Sanchez <roberto.sanchez@formos.com>
2022-05-06 10:09:36 -0500

    Initial commit

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ import {
   Text,
   useColorScheme,
   View,
-  Button
+  Button,
 } from 'react-native';
 
 import {
@@ -27,12 +27,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { EscCharacters } from './symbols';
-
-
-
-
-
+import {EscCharacters} from './modules/esc-pos-parser/symbols';
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -67,80 +62,76 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-
-
-  const server = TcpSocket.createServer(function(socket) {
-    socket.on('data', (data) => {
+  const server = TcpSocket.createServer(function (socket) {
+    socket.on('data', data => {
       socket.write('Echo server ' + data);
     });
-  
-    socket.on('error', (error) => {
+
+    socket.on('error', error => {
       console.log('An error ocurred with client socket ', error);
     });
-  
-    socket.on('close', (error) => {
+
+    socket.on('close', error => {
       console.log('Closed connection with ', socket.address());
     });
+  }).listen({port: 9100, host: '0.0.0.0'});
 
-   
-  }).listen({ port: 9100, host: '0.0.0.0' });
-  
-
-  const decode = (data) =>{
-    let resp = "";
-    for(var d of data){
+  const decode = data => {
+    let resp = '';
+    for (var d of data) {
       const letter = String.fromCharCode(d);
-      if(!EscCharacters.includes(letter)){
+      if (!EscCharacters.includes(letter)) {
         resp += letter;
       }
     }
-    
+
     return resp;
-  }
+  };
 
-  function toHex(str,hex){
-    try{
+  function toHex(str, hex) {
+    try {
       hex = unescape(encodeURIComponent(str))
-      .split('').map(function(v){
-        return v.charCodeAt(0).toString(16)
-      }).join('')
+        .split('')
+        .map(function (v) {
+          return v.charCodeAt(0).toString(16);
+        })
+        .join('');
+    } catch (e) {
+      hex = str;
+      console.log('invalid text input: ' + str);
     }
-    catch(e){
-      hex = str
-      console.log('invalid text input: ' + str)
-    }
-    return hex
+    return hex;
   }
-  
 
-  server.on('connection', (socket) => {
-    console.log('Client connected to server on ' + JSON.stringify(socket.address()));
+  server.on('connection', socket => {
+    console.log(
+      'Client connected to server on ' + JSON.stringify(socket.address()),
+    );
 
-    socket.on('data', (data) => {
-        // console.log('Server client received: ' + (data.length < 2500 ? data : data.length + ' bytes'));
-        console.log('Server client received: ' + (data.length < 25000 ? decode(data) : data.length + ' bytes'));
+    socket.on('data', data => {
+      // console.log('Server client received: ' + (data.length < 2500 ? data : data.length + ' bytes'));
+      console.log(
+        'Server client received: ' +
+          (data.length < 25000 ? decode(data) : data.length + ' bytes'),
+      );
     });
 
-    socket.on('error', (error) => {
-        console.log('Server client error ' + error);
+    socket.on('error', error => {
+      console.log('Server client error ' + error);
     });
 
-    socket.on('close', (error) => {
-        console.log('Server client closed ' + (error ? error : ''));
+    socket.on('close', error => {
+      console.log('Server client closed ' + (error ? error : ''));
     });
-});
+  });
 
-  server.on('error', (error) => {
+  server.on('error', error => {
     console.log('An error ocurred with the server', error);
   });
-  
+
   server.on('close', () => {
     console.log('Server closed connection');
   });
-
-  
-
-
 
   return (
     <SafeAreaView style={backgroundStyle}>
