1387807c2a2017910157d39ea7ad50514538f3d0
Andres Ordonez <andres.ordonez@formos.com>
2022-05-06 14:48:35 -0500

    Created file for commands

--- modules/esc-pos-parser/commandsAndres.js
+++ modules/esc-pos-parser/commandsAndres.js
@@ -1,7 +1,7 @@
+import rc from './regexContants';
 const s = require('./symbols');
-const rc = require('./regexContants');
 
 const { EscPosCommands } = require('./commands');
 
 // Basic control command
-export const ESC_EXCLAMATION_MARK_N = new EscPosCommands(s.ESC + s.EXCLAMATION_MARK + rc.NUMBER_0_TO_255, '', 'Select character printing mode');
+export const ESC_EXCLAMATION_MARK_N = new EscPosCommands(s.ESC.symbol + s.EXCLAMATION_MARK_CLOSE.symbol + rc.NUMBER_0_TO_255, '', 'Select character printing mode');
