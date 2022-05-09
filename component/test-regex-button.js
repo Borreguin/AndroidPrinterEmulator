92844fb596cf67e9db43b92b6dd4abb930199556
Andres Ordonez <andres.ordonez@formos.com>
2022-05-09 09:53:42 -0500

    Cleaned code

--- component/test-regex-button.js
+++ component/test-regex-button.js
@@ -9,10 +9,10 @@ const TextRegexButton = () => {
     return (
         <Button title='Press me' onPress={() => {
             const text = '!214464565';
-            const a = ESC_EXCLAMATION_MARK_N.command.test(text)
-            const newStr = text.replace(ESC_EXCLAMATION_MARK_N.command, "-");
+            const textMatchs = ESC_EXCLAMATION_MARK_N.command.test(text)
+            const replaceTextByRegex = text.replace(ESC_EXCLAMATION_MARK_N.command, "");
 
-            console.log(a, text, newStr)
+            console.log(textMatchs, text, replaceTextByRegex)
         }} />
     )
 }
