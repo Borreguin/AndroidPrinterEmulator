855493970a43432f91e97d08df8f9d550ad79e6c
Mirelly Recalde <mirelly.recalde@formos.com>
2022-05-19 10:19:02 -0500

    Remove log

--- app/views/DevTool/DevTool.js
+++ app/views/DevTool/DevTool.js
@@ -45,7 +45,6 @@ export const DevTool = ({ buffer, setBuffer }) => {
         const today = new Date().toISOString().replace(/[:]/g, "_");
         const pathToWrite = `${RNFetchBlob.fs.dirs.DownloadDir}/printerEmulator-${today}.csv`;
 
-        console.log(pathToWrite)
         RNFetchBlob.fs
             .writeFile(pathToWrite, jsonData, 'utf8')
             .then(() => {
